2df960cf26cc08c0a83dad71ac37c4c1a0170687
unknown <mitridat78@gmail.com>
2022-04-25 14:27:34 +0300

    add new task

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("Hello Word!");
+# print("Hello Word!")
+# name = input("You name? ")
+# print(f"Hi, {name}")
+import sys
+
+try:
+    minute = int(input("Minute (N): "))
+#    number2 = int(input ("Яблук (K): "))
+except ValueError:
+    print("Помилка!!!")
+    sys.exit(1)
+
+print(f"Min: {minute % 60} Hour: {minute // 60}")
+#print(f"Залишилось: {number2 % number1}")
+#print(f"Result: {number1 + number2}")
