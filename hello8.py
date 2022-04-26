fe211977717e6c2e55ba59299443e2bc7f0e04d5
unknown <mitridat78@gmail.com>
2022-04-26 15:56:14 +0300

    add new rule

---
+++ hello8.py
@@ -0,0 +1,10 @@
+#Дано натуральное число. Выведите его последнюю цифру. 
+number1 = int(input("Input Number: "))
+print(f"Остання цифра: {number1 % 10}")
+#Дано положительное действительное число X. Выведите его дробную часть. 
+from math import *
+number1 = float(input("Input Number: "))
+if number1 - int(number1) > 0:
+    print(f"Дробна частина: {round(number1 - int(number1), 2)}")
+else:
+    print(f"Дробна частина: 0")
