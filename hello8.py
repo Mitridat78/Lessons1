a28ecd2404322440dcc45369564bc29683da6582
unknown <mitridat78@gmail.com>
2022-04-26 15:58:10 +0300

    add new comments

--- hello8.py
+++ hello8.py
@@ -4,7 +4,7 @@ print(f"Остання цифра: {number1 % 10}")
 #Дано положительное действительное число X. Выведите его дробную часть. 
 from math import *
 number1 = float(input("Input Number: "))
-if number1 - int(number1) > 0:
+if number1 - int(number1) > 0: # Якщо саме мінус ціла його частина більше за нуль
     print(f"Дробна частина: {round(number1 - int(number1), 2)}")
 else:
     print(f"Дробна частина: 0")
