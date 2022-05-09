eac0743cbf729dc948bc79f3102c726219f49476
unknown <mitridat78@gmail.com>
2022-05-09 18:28:35 +0300

    add new task

--- hello49.py
+++ hello49.py
@@ -4,8 +4,17 @@
 """
 a = [int(input(f"Введіть {i} елемент списку: ")) for i in range(int(input("Введіть кількість елементів списку: ")))]
 
-#for i in range(0, len(a)):
-print(a.index(max(a)))
-print(a.index(min(a)))
+for elem in a:
+    max1 = max(a)
+    min1 = min(a)
 
-
+for i in range(len(a)):
+    if a[i] == max1:
+        a[i] = min1
+    elif a[i] == min1:
+        a[i] = max1
+print(a[::])
+# короткий варіант
+for i in range(len(a)):
+    a[a.index(max(a))], a[a.index(min(a))] = min(a), max(a)
+print(a, sep = ', ')
