441d63457bd7b9a49e82f4eeee9dbba1fefacf61
unknown <mitridat78@gmail.com>
2022-05-20 13:53:37 +0300

    new task

--- hello69.py
+++ hello69.py
@@ -5,15 +5,17 @@
 не встречалось..
 """
 
-numbers = [int(i) for i in input().split()]
-"""entered = list()
-for elem in numbers:
-    if elem in entered:
+a = [int(i) for i in input().split()]
+b = []
+for elem in a:
+    if elem in b:
         print("YES")
     else:
-        entered.append(elem)
+        b.append(elem)
         print("NO")
-"""
-for i in range(len(numbers)):
-    if numbers[i+1] in numbers[0:i-1]:
+# другий спосіб
+for i in range(len(a)):
+    if a[i] in a[:i]:
         print("yes")
+    else:
+        print("no")
